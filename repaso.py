7221d0269f1d89587de6c36af45aa74742af5421
esruffini <esruffini@gmail.com>
2026-03-11 21:33:31 -0300

    Mi primer commit

---
+++ repaso.py
@@ -0,0 +1,24 @@
+"""comentario en linea"""
+
+print("calculadora en Python")
+
+#inicializar variables
+
+#numero1 = 4
+
+#numero2= 6
+
+
+
+numero1 = input("Ingrese un numero")
+
+numero2= input("Ingrese un numero")
+
+#sumar los numeros
+
+suma = numero1 + numero2
+
+#mostrar resultado
+
+print(suma)
+
